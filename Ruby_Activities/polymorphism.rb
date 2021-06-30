44b447edede8f47f5d230b49207fd5b5800d7345
Em Jen P Arce <ejparce@IEmAwesome.localdomain>
2021-06-30 13:43:52 +0800

    revise lines lines 70 onwards

--- Ruby_Activities/polymorphism.rb
+++ Ruby_Activities/polymorphism.rb
@@ -46,6 +46,7 @@ puts blissful.fight
 puts "========END OF POLYMORPHISM ASSIGNMENT (1/2)=========="
 
 #END OF POLYMORPHISM ASSIGNMENT
+
 #START OF DUCK TYPING ASSIGNMENT
 class Psychic
 	def say
@@ -68,37 +69,28 @@ class Fairy
 end 
 
 class PokemonActivities
-	attr_accessor :monsters
-	def initialize
-		@monsters = []
-		psychic = Psychic.new()
-		fairy = Fairy.new()
-		@monsters.push(psychic)
-		@monsters.push(fairy)
+	def say(monster)
+		monster.say
 	end
 
-	def say
-		monsters.each do |monster|
-			monster.say
-		end		
-	end
-
-	def battle
-		monsters.each do |monster|
+	def battle(monster)
 			monster.battle
-		end		
 	end
 end
 
 puts "========START OF POLYMORPHISM ASSIGNMENT (2/2)=========="
 activities = PokemonActivities.new
-puts activities.say
-puts activities.battle
+puts "Using Psychic Object"
+puts activities.say(Psychic.new)
+puts activities.battle(Psychic.new)
+puts "Using Fairy Object"
+puts activities.say(Fairy.new)
+puts activities.battle(Fairy.new)
 puts "========END OF POLYMORPHISM ASSIGNMENT (2/2)=========="
 #END OF DUCK TYPING ASSIGNMENT
 #Notes for me:
-#1. 2 pokemon monster types (2 objects) with 2 methods of same name but behave differently. PokemonActivities is the class which aggregates the methods of 2 objects.
-#2. in PokemonActivities we create 2 instances of psychic and fairy objects and place in monsters array
-#3. Achieve duck typing by doing say and battle which are also present in 2 objects.  
+#1. 2 pokemon monster types (2 objects) with 2 methods of same name but behave differently. PokemonActivities is the class which aggregates(public interface) the methods of 2 objects.
+#2. in PokemonActivities we create instances of psychic and fairy objects and pass these objs as arguments to do say and battle methods
+#3. Achieve duck typing by doing step 2
 #4. There are 2 objects with 2 methods with same name but different behavior. No need to do _kindof? as both objects shares a public interface (same set of methods). Useful to make code cleaner by avoiding if else and switch
-#5. Duck typing places importance on capabilities of object and less on its type (class). What object can do do and not what it is.
+#5. Duck typing emphasizes on capabilities of object and less on its type (class). What object can do and not what it is.
